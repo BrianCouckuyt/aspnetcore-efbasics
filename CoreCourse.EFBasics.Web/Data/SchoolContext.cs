dbdc0308fab250bf88841391a8aae636fb50beec
Brian Couckuyt <brian.couckuyt@gmail.com>
2018-05-20 08:27:22 +0200

    started work

---
+++ CoreCourse.EFBasics.Web/Data/SchoolContext.cs
@@ -0,0 +1,20 @@
+﻿using CoreCourse.EFBasics.Web.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCourse.EFBasics.Web.Data
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(DbContextOptions<SchoolContext> options) :base(options)
+        {
+
+        }
+
+        public DbSet<Teacher> Teachers { get; set; }
+        public DbSet<Course> Courses { get; set; }
+    }
+}
---
+++ CoreCourse.EFBasics.Web/Entities/Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreCourse.EFBasics.Web.Entities
+{
+    public class Teacher
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public decimal YearlyWage { get; set; }
+        public ICollection<Course> Courses { get; set; }
+    }
+}
